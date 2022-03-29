afe8c93c3fe0541ea7017a4f1172a5615f1d932b
KrzQbt <62937499+KrzQbt@users.noreply.github.com>
2022-03-29 13:36:57 +0200

    Update auth.interceptor.ts

--- scrum-front-angular/src/app/services/auth-interceptor/auth.interceptor.ts
+++ scrum-front-angular/src/app/services/auth-interceptor/auth.interceptor.ts
@@ -10,8 +10,8 @@ import { Observable } from 'rxjs';
 
 import { TokenStorageService } from '../token/token-storage.service';
 
-// const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
-const TOKEN_HEADER_KEY = 'x-access-token';   // for Node.js Express back-end
+
+const TOKEN_HEADER_KEY = 'x-access-token';  
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -22,10 +22,7 @@ export class AuthInterceptor implements HttpInterceptor {
     let authReq = request;
     const token = this.token.getToken();
     if (token != null) {
-      // for Spring Boot back-end
-      // authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
-
-      // for Node.js Express back-end
+      
       authReq = request.clone({ headers: request.headers.set(TOKEN_HEADER_KEY, token) });
     }
     return next.handle(authReq);
