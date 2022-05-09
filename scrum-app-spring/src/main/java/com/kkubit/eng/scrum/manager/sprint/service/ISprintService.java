c63d9f2bfaa37870e037bcf766b777fb4265254c
KrzQbt <62937499+KrzQbt@users.noreply.github.com>
2022-05-09 19:17:32 +0200

    Update ISprintService.java

--- scrum-app-spring/src/main/java/com/kkubit/eng/scrum/manager/sprint/service/ISprintService.java
+++ scrum-app-spring/src/main/java/com/kkubit/eng/scrum/manager/sprint/service/ISprintService.java
@@ -8,19 +8,19 @@ import java.util.LinkedList;
 
 public interface ISprintService {
 
-    LinkedList<Sprint> getMySprints(Principal principal);
-    LinkedList<Task> getTaskListForSprint(Long sprintId);
+    List<Sprint> getMySprints(Principal principal);
+    List<Task> getTaskListForSprint(Long sprintId);
     void updateTask(Task task);
     void updateTaskStatus(TaskStatusPatch statusPatch);
     void createTask(Task task);
     void deleteTask(Long id);
     BacklogItem getBacklogItemByPickedId( Long itemId);
-    LinkedList<BacklogItem> getPickedItems(Long sprintId);
-    LinkedList<PickedItem> getAlreadyPickedItems(Long sprintId);
+    List<BacklogItem> getPickedItems(Long sprintId);
+    List<PickedItem> getAlreadyPickedItems(Long sprintId);
     void pickItemsForSprint(LinkedList<BacklogItem> backlogItems, Long sprintId);
     void updateSprintItems(LinkedList<PickedItem> pickedItems);
     Sprint getSprint(Long sprintId);
-    LinkedList<Sprint> getAllSprintsInProject(Long projectId);
+    List<Sprint> getAllSprintsInProject(Long projectId);
     void createSprint(Long projectId, NewSprintWrapper sprint, String username);
     void changeSprintStatus(Long sprintId, SprintStatusPatch statusPatch);
     void updateSprint(Long sprintId, Sprint sprint);
